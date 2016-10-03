ff2cdcd9af38be3cc6ca37af3247ad26ea255ffb
Julian Löhr <julian.loehr@outlook.com>
2016-10-03 20:34:52 +0200

    Change Head Tracker to track left eye instead of nose

--- AugmentedMagicMirror/HeadTracker.cpp
+++ AugmentedMagicMirror/HeadTracker.cpp
@@ -14,7 +14,7 @@ HeadTracker::HeadTracker(_In_ Camera & Camera, _In_ ::Kinect & Kinect)
 
 void HeadTracker::FaceModelUpdatedCallback(_In_ const Kinect::CameraSpacePointList & FaceVertices, _In_ const Vector3 & Offset, _In_ const float & RealWorldToVirutalScale)
 {	
-	const CameraSpacePoint & NoseTop = FaceVertices[HighDetailFacePoints_NoseTop];
+	const CameraSpacePoint & NoseTop = FaceVertices[HighDetailFacePoints_LefteyeInnercorner]; //HighDetailFacePoints_NoseTop
 
 	BoundCamera.UpdateCamera(Vector3((NoseTop.X * RealWorldToVirutalScale) + Offset.X, (NoseTop.Y * RealWorldToVirutalScale) + Offset.Y, (NoseTop.Z * RealWorldToVirutalScale) + Offset.Z));
 }
